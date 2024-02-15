2f2c1f3411f407ab68e7dc21429f342c64118161
hosseinfeiz <hfa.zadeh.k@gmail.com>
2024-02-15 11:44:19 -0500

    tracking working properly

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEVICE = '0' # For GPU set device num which you want to use (or set 'cpu', but i
 #DEVICE = 'cpu'
 
 # Our confidence for every person (bbox)
-PERSON_CONF = 0.4
+PERSON_CONF = 0.6
 
 KEYPOINTS = ["nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
 
@@ -16,13 +16,13 @@ IOU_THRESHOLD = 0.1
 # It's xMem original config, you can try to change this values for your task (check xMem article)
 XMEM_CONFIG = {
     'top_k': 30,
-    'mem_every': 30,
-    'deep_update_every': -1,
+    'mem_every': 5,
+    'deep_update_every': 1000,
     'enable_long_term': True,
     'enable_long_term_count_usage': True,
     'num_prototypes': 256,
-    'min_mid_term_frames': 2,
-    'max_mid_term_frames': 30,
+    'min_mid_term_frames': 5,
+    'max_mid_term_frames': 10,
     'max_long_term_elements': 1000,
 }
 
@@ -30,7 +30,7 @@ XMEM_CONFIG = {
 MAX_OBJECT_CNT = 20
 
 # Check new persons in frame every N frames
-YOLO_EVERY = 2
+YOLO_EVERY = 1
 
 # Resize processed video. For better results you can increase resolution
-INFERENCE_SIZE = (1280, 720)
+INFERENCE_SIZE = (1920, 1080)
--- run_tracking.sh
+++ run_tracking.sh
@@ -6,7 +6,7 @@ for folder in "${folders[@]}"
         for file in  1 2 3 4
         do 
 
-            python tracking/tracking.py  --file "Dataset/new/$folder/videos/$file.mp4" --output_video_path="Dataset/new/$folder/vis_$file.mp4"
+            python tracking.py  --video_path "Dataset/new/$folder/1001_2000/videos/$file.mp4" --output_video_path="Dataset/new/$folder/1001_2000/vis_$file.mp4"
         done
     
 done
